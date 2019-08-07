ab137ebb94b9326549b2d460447c770af432a153
lovelope <lovelope@qq.com>
2019-08-07 22:59:13 +0800

    chore: stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,12 +2,25 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
   plugins: ['stylelint-order', 'stylelint-config-rational-order/plugin'],
   rules: {
+    // `standard` conflict with `rational-order`
+    'declaration-empty-line-before': [
+      'never',
+      {
+        except: ['after-comment', 'after-declaration', 'first-nested'],
+        ignore: [
+          'after-comment',
+          'after-declaration',
+          'first-nested',
+          'inside-single-line-block',
+        ],
+      },
+    ],
     'order/properties-order': [],
     'plugin/rational-order': [
       true,
       {
         'border-in-box-model': false,
-        'empty-line-between-groups': false,
+        'empty-line-between-groups': true,
       },
     ],
   },
